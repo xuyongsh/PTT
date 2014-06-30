6e46dc9eeceb4e8594b1a2b3175036ee892209b3
TheoWang <smartwxh@gmail.com>
2014-06-30 10:08:10 +0800

    Initial version.. Theo Wang

---
+++ PTT/src/com/cnx/ptt/DisplayMessageActivity.java
@@ -0,0 +1,74 @@
+package com.cnx.ptt;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DisplayMessageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_display_message);
+		super.setTitle("Theo Wang");
+		
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+		
+		// Get the message from the intent
+	    //Intent intent = getIntent();
+	    
+	    
+		
+//		 requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+//		 setContentView(R.layout.custom_title); 
+//		 getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,      
+//				   "Theo Wang");   
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.display_message, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_display_message,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
